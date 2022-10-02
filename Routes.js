90d5d1211293d1a6efc8a040240909f984c8cbfb
Bryce Speirs <baspeirs@gmail.com>
2022-10-02 18:37:49 -0400

    Update Routes.js

--- Routes.js
+++ Routes.js
@@ -87,68 +87,6 @@ router.get("/api/store_schedule", async (req, res) => {
 
 // ============ setting up OAth for data validation querry to google sheet ========== 
 
-// const postDataValidation = async (employeeNames) => {
-//     const clientID = "702908600191-vvp7ri26ivi6uaebdtlovtss7t01k43d.apps.googleusercontent.com";
-//     const clientSecret = "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";
-//     const accessCode = "4/0ARtbsJpr7Et3T0vl88a8HQtp0dVIMf0Itpyz2M62u4QcWMPfayTbOY9QgkpAN_uyYCM-Pw";
-//     const refreshToken = "1//01rwJPNtO5A7iCgYIARAAGAESNwF-L9IrGYfPW5AxdmdhaTLrGvlgyWICEao0vNQ3kyNsFDdczsJfGY4VimDMfUZ-FuyE8YqmFGs"
-
-    
-
-
-
-
-//     const refreshAccessToken = (refreshTkn) => {
-//         url="https://accounts.google.com/o/oath2/token"
-//         const data={
-//             "grant_type": "refresh_token",
-//             "client_id": clientID,
-//             "client_secret": clientSecret,
-//             "refresh_token": refreshTkn
-//         }
-//         const headers = {
-//             "content-type": "application/x-www-form-urlencoded"
-//         }
-
-//         const r = router.post("POST", url, data, headers)
-//         return r
-//     }
-
-//     // const sheets = google.sheets({
-//     //     version: "v4",
-//     //     auth: process.env.GOOGLE_ACCOUNT_API_KEY
-//     // });
-//     const sheets = google.sheets({
-//         version: "v4",
-//         auth: refreshAccessToken(refreshToken)
-//     });
-//         await sheets.spreadsheets.batchUpdate({
-//             spreadsheetId: process.env.SPREADSHEET_ID,
-//             setDataValidation: {
-//                 range: {
-//                     sheetId: 1738357002,
-//                     startRowIndex: 1,
-//                     endRowIndex: 7,
-//                     startColumnIndex: 2,
-//                     endColumnIndex: 12
-//                 },
-//                 rule: {
-//                     condition: {
-//                         type: "ONE_OF_LIST",
-//                         values: employeeNames
-//                         // [
-//                         //     {
-//                         //         "userEnteredValue": employeeNames 
-//                         //     }
-//                         // ]
-//                     },
-//                     inputMessage: "Please select a valid option",
-//                     strict: true,
-//                     showCustomUi: false
-//                 }
-//             }
-//         })
-// }
 
 // ============= User Routes For Database ==================
 router.post("/api/register", (req, res) => {
@@ -213,22 +151,6 @@ router.get("/api/directory", (req, res) => {
             res.json(result)
         })
 })
-
-// router.get("/api/directoryForSheets", (req, res) => {
-//     db.User.find()
-//         .then(result => {
-//             res.json(result)
-//             let managerList = []
-//             result.forEach(employee => {
-//                 if (employee.manager) {
-//                     console.log(employee.name)
-//                     const empObj = { "userEnteredValue": employee.name }
-//                     managerList.push(empObj)
-//                 }
-//             })
-//             postDataValidation(managerList)
-//         })
-// });
 
 
 module.exports = router;
