03e6c8807b527a6e49d0cc2aed15919dac93bf84
Bryce Speirs <baspeirs@gmail.com>
2022-10-02 19:50:55 -0400

    styling for nav bar

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../Styles/NavBar.css"
 
 export default function NavBar(props) {
-    return (
-        <div className="navBar">
-            <nav>
-            <ul>
-              <li>
-                <Link to="/storeSchedule">Store Schedule</Link>
-              </li>
-              <li>
-                <Link to="/individualSchedule">Individual Schedule</Link>
-              </li>
-              <li>
-                <Link to="/employeeOps">Employee Ops</Link>
-              </li>
-              <li>
-                <button onClick={props.logout}>Log Out</button>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        // class to add forselected page: className="is-active"
-    )
+  return (
+    <div className="navBar">
+      <img src={require("../Images/ScheduleTrackerPro.png")} id="logo-nav" alt="logo" />
+      <div id="nav-button-holder">
+        <button className='nav-item'><Link to="/storeSchedule">Store Schedule</Link></button>
+        <button className='nav-item'><Link to="/individualSchedule">Individual Schedule</Link></button>
+        <button className='nav-item'><Link to="/employeeOps">Employee Ops</Link></button>
+        <button className='nav-item' onClick={props.logout}>Log Out</button>
+      </div>
+    </div>
+    // class to add forselected page: className="is-active"
+  )
 }
